1603bd7c142f87548714706c92aa0e1057bd2f2e
Josh Adams <josh@isotope11.com>
2013-04-15 20:10:52 -0500

    Flesh out the Motor spec

--- lib/raspi_tank/motor.rb
+++ lib/raspi_tank/motor.rb
@@ -1,9 +1,55 @@
 module RaspiTank
   class Motor
+    attr_reader :direction, :speed
+
     def initialize(options={})
-      @forward  = options.fetch(:forward){  raise "forward must be specified"  }
-      @backward = options.fetch(:backward){ raise "backward must be specified" }
-      @enable   = options.fetch(:enable){   raise "enable must be specified"   }
+      @forward   = options.fetch(:forward){  raise "forward must be specified"  }
+      @backward  = options.fetch(:backward){ raise "backward must be specified" }
+      @enable    = options.fetch(:enable){   raise "enable must be specified"   }
+      @direction = :forward
+      @speed     = 0
+    end
+
+    def forward_pin
+      @forward_pin ||= PiBlasterInterface.new(@forward)
+    end
+
+    def backward_pin
+      @backward_pin ||= PiBlasterInterface.new(@backward)
+    end
+
+    def enable_pin
+      @enable_pin ||= PiBlasterInterface.new(@enable)
+    end
+
+    def go_backward
+      self.direction = :backward
+      forward_pin.value = 0
+      backward_pin.value = 1
+    end
+
+    def go_forward
+      self.direction = :forward
+      forward_pin.value = 1
+      backward_pin.value = 0
+    end
+
+    def speed=(new)
+      validate_speed(new)
+      @speed = new
+      enable_pin.value = new
+    end
+
+    protected
+    attr_writer :direction
+
+    def validate_speed(new)
+      return true if valid_speeds.cover?(new)
+      raise OutOfRangeException, "Invalid speed: #{new}"
+    end
+
+    def valid_speeds
+      (0..1)
     end
   end
 end
--- spec/motor_spec.rb
+++ spec/motor_spec.rb
@@ -1,8 +1,52 @@
 require_relative 'spec_helper'
 
 describe Motor do
+  before do
+    @motor = Motor.new(forward: 11, enable: 12, backward: 13)
+  end
+
   it "can be initialized with specified pins" do
-    motor = Motor.new(forward: 11, enable: 12, backward: 13)
-    expect(motor).to be_a Motor
+    expect(@motor).to be_a Motor
+  end
+
+  it "creates a PiBlasterInterface for each pin" do
+    expect(@motor.forward_pin).to be_a(PiBlasterInterface)
+    expect(@motor.enable_pin).to be_a(PiBlasterInterface)
+    expect(@motor.backward_pin).to be_a(PiBlasterInterface)
+  end
+
+  it "starts out going forward" do
+    expect(@motor.direction).to eq(:forward)
+  end
+
+  it "can have its direction specified" do
+    @motor.go_backward
+    expect(@motor.direction).to eq(:backward)
+    @motor.go_forward
+    expect(@motor.direction).to eq(:forward)
+  end
+
+  it "starts out with all pins low" do
+    expect(@motor.forward_pin.value).to eq(0)
+    expect(@motor.enable_pin.value).to eq(0)
+    expect(@motor.backward_pin.value).to eq(0)
+  end
+
+  it "sets forward pin high and backward pin low when going forward" do
+    @motor.go_forward
+    expect(@motor.forward_pin.value).to eq(1)
+    expect(@motor.backward_pin.value).to eq(0)
+  end
+
+  it "sets forward pin low and backward pin high when going backward" do
+    @motor.go_backward
+    expect(@motor.forward_pin.value).to eq(0)
+    expect(@motor.backward_pin.value).to eq(1)
+  end
+
+  it "sets enable pin value to its speed value" do
+    @motor.speed = 1
+    expect(@motor.speed).to eq(1)
+    expect(@motor.enable_pin.value).to eq(1)
   end
 end
