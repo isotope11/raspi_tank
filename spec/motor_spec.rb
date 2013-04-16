8aa712bd0f78c037192730860e5cb3caded9f838
Josh Adams <josh@isotope11.com>
2013-04-15 19:27:18 -0500

    Add initial motor test

---
+++ spec/motor_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+
+module RaspiTank
+  class Motor
+    def initialize(options={})
+      @forward  = options.fetch(:forward){  raise "forward must be specified"  }
+      @backward = options.fetch(:backward){ raise "backward must be specified" }
+      @enable   = options.fetch(:enable){   raise "enable must be specified"   }
+    end
+  end
+end
+
+include RaspiTank
+
+describe Motor do
+  it "can be initialized with specified pins" do
+    motor = Motor.new(forward: 11, enable: 12, backward: 13)
+    motor.should be_a Motor
+  end
+end
